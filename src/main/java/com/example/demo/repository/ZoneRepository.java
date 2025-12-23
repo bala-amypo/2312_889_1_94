918186c1a2029161da9e0a82c70db488c871fc74
bala-amypo <bala@amypo.in>
2025-12-23 04:07:22 +0000

    User commit

--- src/main/java/com/example/demo/repository/ZoneRepository.java
+++ src/main/java/com/example/demo/repository/ZoneRepository.java
@@ -2,8 +2,11 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Zone;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface ZoneRepository extends JpaRepository<Zone, Long> {
     Optional<Zone> findByZoneName(String name);
 }
